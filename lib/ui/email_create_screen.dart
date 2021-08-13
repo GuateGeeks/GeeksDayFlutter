984a388323788a2359c43504bcd47929ab9ec456
Jhonproyect <albertcasti0919@gmail.com>
2021-08-12 21:16:32 -0600

    Ticket #3 update of the registration form design

--- lib/ui/email_create_screen.dart
+++ lib/ui/email_create_screen.dart
@@ -29,55 +29,86 @@ class _EmailCreateState extends State<EmailCreate> {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double maxWidth = width > 500 ? 500 : width;
     return Scaffold(
         appBar: AppBar(title: Text('Create account')),
+        backgroundColor: Color.fromRGBO(220, 220, 220, 1),
         body: BlocBuilder<AuthCubit, AuthState>(
           builder: (_, state) {
-            return Form(
-              key: _formKey,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
+            return Center(
+              child: Container(
+                width: maxWidth,
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    if (state is AuthSigningIn)
-                      Center(child: CircularProgressIndicator()),
-                    if (state is AuthError)
-                      Text(
-                        state.message,
-                        style: TextStyle(color: Colors.red, fontSize: 24),
-                      ),
-                    SizedBox(height: 8),
-                    TextFormField(
-                      controller: _emailController,
-                      decoration: InputDecoration(labelText: 'Email'),
-                      validator: emailValidator,
+                    SizedBox(height: 30.0),
+                    Text(
+                      "Sing Up",
+                      style: TextStyle(
+                          fontSize: 35.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
                     ),
-                    SizedBox(height: 8),
-                    TextFormField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(labelText: 'Password'),
-                      validator: passwordValidator,
+                    SizedBox(
+                      height: 50,
                     ),
-                    SizedBox(height: 8),
-                    TextFormField(
-                      controller: _repeatPasswordController,
-                      decoration: InputDecoration(labelText: 'Repeat Password'),
-                      validator: passwordValidator,
-                    ),
-                    Center(
-                      child: ElevatedButton(
-                        child: const Text('Create'),
-                        onPressed: () {
-                          if (_formKey.currentState?.validate() == true) {
-                            context
-                                .read<AuthCubit>()
-                                .createUserWithEmailAndPassword(
-                                  _emailController.text,
-                                  _passwordController.text,
-                                );
-                          }
-                        },
+                    Form(
+                      key: _formKey,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 50),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            if (state is AuthSigningIn)
+                              Center(child: CircularProgressIndicator()),
+                            if (state is AuthError)
+                              Text(
+                                state.message,
+                                style:
+                                    TextStyle(color: Colors.red, fontSize: 24),
+                              ),
+                            SizedBox(height: 8),
+                            //Show input email
+                            email(),
+                            //Show input Username
+                            SizedBox(height: 8),
+                            username(),
+                            SizedBox(height: 8),
+                            //Show input Password
+                            password(),
+
+                            SizedBox(height: 8),
+                            //Show input Repear Password
+                            repeatPassword(),
+                            SizedBox(height: 18),
+                            Center(
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  padding: MaterialStateProperty.all(
+                                      EdgeInsets.symmetric(vertical: 18)),
+                                ),
+                                child: Center(
+                                    child: Text(
+                                  'Sing Up',
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                  ),
+                                )),
+                                onPressed: () {
+                                  if (_formKey.currentState?.validate() ==
+                                      true) {
+                                    context
+                                        .read<AuthCubit>()
+                                        .createUserWithEmailAndPassword(
+                                          _emailController.text,
+                                          _passwordController.text,
+                                        );
+                                  }
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
@@ -86,5 +117,160 @@ class _EmailCreateState extends State<EmailCreate> {
             );
           },
         ));
+  }
+
+//Create input Email
+  Widget email() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 3),
+          child: Text(
+            "Email",
+            style: TextStyle(
+                fontWeight: FontWeight.w600, color: Colors.white, fontSize: 18),
+          ),
+        ),
+        TextFormField(
+          keyboardType: TextInputType.name,
+          decoration: InputDecoration(
+            hintText: "Enter your email",
+            border: InputBorder.none,
+            filled: true,
+            fillColor: Color.fromRGBO(240, 240, 240, 1),
+            contentPadding:
+                const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: new BorderRadius.circular(8),
+              borderSide: new BorderSide(
+                color: Color.fromRGBO(240, 240, 240, 1),
+              ),
+            ),
+            enabledBorder: UnderlineInputBorder(
+              borderRadius: new BorderRadius.circular(8),
+              borderSide: new BorderSide(
+                color: Color.fromRGBO(240, 240, 240, 1),
+              ),
+            ),
+          ),
+          validator: emailValidator,
+        ),
+      ],
+    );
+  }
+
+//Create input Username
+  Widget username() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 3),
+          child: Text(
+            "Username",
+            style: TextStyle(
+                fontWeight: FontWeight.w600, color: Colors.white, fontSize: 18),
+          ),
+        ),
+        TextFormField(
+          keyboardType: TextInputType.name,
+          controller: _emailController,
+          cursorColor: Color.fromRGBO(170, 170, 170, 1),
+          decoration: InputDecoration(
+            hintText: "Enter your username",
+            border: InputBorder.none,
+            filled: true,
+            fillColor: Color.fromRGBO(240, 240, 240, 1),
+            contentPadding:
+                const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: new BorderRadius.circular(8),
+              borderSide: new BorderSide(
+                color: Color.fromRGBO(240, 240, 240, 1),
+              ),
+            ),
+            enabledBorder: UnderlineInputBorder(
+              borderRadius: new BorderRadius.circular(8),
+              borderSide: new BorderSide(
+                color: Color.fromRGBO(240, 240, 240, 1),
+              ),
+            ),
+          ),
+          validator: emailValidator,
+        ),
+      ],
+    );
+  }
+
+//Create input Password
+  Widget password() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 3),
+          child: Text(
+            "Password",
+            style: TextStyle(
+                fontWeight: FontWeight.w600, color: Colors.white, fontSize: 18),
+          ),
+        ),
+        inputsPassword(
+            _passwordController, "Password", passwordValidator, true),
+      ],
+    );
+  }
+
+//Create input RepeatPassword
+  Widget repeatPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 3),
+          child: Text(
+            "Repear Password",
+            style: TextStyle(
+                fontWeight: FontWeight.w600, color: Colors.white, fontSize: 18),
+          ),
+        ),
+        inputsPassword(_repeatPasswordController, "Repeat Password",
+            passwordValidator, true),
+      ],
+    );
+  }
+
+//This function creates the layout of the input password, and repeatpassword
+  Widget inputsPassword(controller, placeholder, validator, showText) {
+    return TextFormField(
+      obscureText: showText,
+      controller: controller,
+      cursorColor: Color.fromRGBO(170, 170, 170, 1),
+      decoration: InputDecoration(
+        suffixIcon: Icon(
+          Icons.visibility,
+        ),
+        hintText: placeholder,
+        border: InputBorder.none,
+        filled: true,
+        fillColor: Color.fromRGBO(240, 240, 240, 1),
+        contentPadding:
+            const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: new BorderRadius.circular(8),
+          borderSide: new BorderSide(
+            color: Color.fromRGBO(240, 240, 240, 1),
+          ),
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderRadius: new BorderRadius.circular(8),
+          borderSide: new BorderSide(
+            color: Color.fromRGBO(240, 240, 240, 1),
+          ),
+        ),
+      ),
+      validator: validator,
+    );
   }
 }
