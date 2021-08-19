c874131ce20371d91435f04872f02fa211b58f0a
Jhonproyec <72222081+Jhonproyec@users.noreply.github.com>
2021-08-19 22:41:31 +0000

    Deploying to gh-pages from @ GuateGeeks/GeeksDayFlutter@0a460cb3ba928a2f7c5fd98e6728efa72d53627b 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,7 +19,7 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "cd30b6ab6774127656ea9c126369b699",
 "version.json": "da2e0573834eb2187048bf7b88f15c46",
-"main.dart.js": "76860bf67c81cc0631a846c78a1e7457",
+"main.dart.js": "963d1ee3b2c7d7a8ff735bf4aa93e814",
 "index.html": "def919ae89fbaf4ed446d2adda969b52",
 "/": "def919ae89fbaf4ed446d2adda969b52"
 };
