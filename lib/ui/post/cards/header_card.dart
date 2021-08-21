506ef6cea99a0e1a513425c0e05f4e51e4cf76d0
Jhonproyect <albertcasti0919@gmail.com>
2021-08-20 22:45:51 -0600

    delete post button

--- lib/ui/post/cards/header_card.dart
+++ lib/ui/post/cards/header_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geeksday/bloc/post_cubit.dart';
+import 'package:geeksday/ui/post/cards/post_options.dart';
 
 class HeaderCard extends StatelessWidget {
   const HeaderCard({Key? key}) : super(key: key);
@@ -11,40 +12,47 @@ class HeaderCard extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Container(
-            width: 50,
-            height: 50,
-            padding: EdgeInsets.all(10),
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.blue,
-              image: DecorationImage(
-                image: NetworkImage(
-                    "https://yt3.ggpht.com/a/AATXAJyPMywRmD62sfK-1CXjwF0YkvrvnmaaHzs4uw=s900-c-k-c0xffffffff-no-rj-mo"),
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-            child: Column(
-              children: <Widget>[
-                Text(
-                  state.getPost()!.username,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.black),
-                ),
-                Text(
-                  "2 days ago",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey,
+          Row(
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                padding: EdgeInsets.all(10),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://yt3.ggpht.com/a/AATXAJyPMywRmD62sfK-1CXjwF0YkvrvnmaaHzs4uw=s900-c-k-c0xffffffff-no-rj-mo"),
+                    fit: BoxFit.fill,
                   ),
                 ),
-              ],
-            ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      state.getPost()!.username,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black),
+                    ),
+                    Text(
+                      "2 days ago",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
+          //TODO: Connect button functionality
+          PostOptions(),
         ],
       ),
     );
---
+++ lib/ui/post/cards/post_options.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class PostOptions extends StatefulWidget {
+  PostOptions({Key? key}) : super(key: key);
+
+  @override
+  _PostOptionsState createState() => _PostOptionsState();
+}
+
+class _PostOptionsState extends State<PostOptions> {
+  @override
+  Widget build(BuildContext context) {
+    //show list of actions that can be performed on the post
+    return PopupMenuButton<String>(
+      itemBuilder: (BuildContext context) {
+        return Constants.choinces.map((String choive) {
+          return PopupMenuItem<String>(
+            value: choive,
+            child: Text(choive),
+          );
+        }).toList();
+      },
+      onSelected: choiceAction,
+    );
+  }
+
+  //function to carry out the selected action
+  void choiceAction(String choice) {
+    print("Eliminando");
+  }
+}
+
+//List of possible actions that can be performed on the post, at the moment it can only be eliminated
+class Constants {
+  static const String Delete = "Eliminar";
+
+  static const List<String> choinces = <String>[
+    Delete,
+  ];
+}
