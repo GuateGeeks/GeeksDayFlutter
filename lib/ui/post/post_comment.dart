b8043b0d36b6f45ff35101dab01d561dd04d705c
Jhonproyect <albertcasti0919@gmail.com>
2021-09-03 17:06:28 -0600

    Ticket #7 responsive design of the comments view

--- lib/ui/post/post_comment.dart
+++ lib/ui/post/post_comment.dart
@@ -12,8 +12,208 @@ class PostComment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Pagina para comentarios"),
+    double width = MediaQuery.of(context).size.width;
+    double maxWidth = width > 700 ? 700 : width;
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Container(
+          width: maxWidth,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: ListView(
+                    children: [
+                      headerComment(),
+                      const Divider(
+                        height: 25,
+                        thickness: 1,
+                      ),
+                      comments(),
+                      comments(),
+                      comments(),
+                      comments(),
+                      SizedBox(
+                        height: 70,
+                      )
+                    ],
+                  ),
+                ),
+                textFormFielComment()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget headerComment() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://yt3.ggpht.com/a/AATXAJyPMywRmD62sfK-1CXjwF0YkvrvnmaaHzs4uw=s900-c-k-c0xffffffff-no-rj-mo"),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "User name",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black),
+                    ),
+                    Text(
+                      "Hace 2 horas",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 7.0,
+          ),
+          Text(
+            "Descripcion del post Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget comments() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://yt3.ggpht.com/a/AATXAJyPMywRmD62sfK-1CXjwF0YkvrvnmaaHzs4uw=s900-c-k-c0xffffffff-no-rj-mo"),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "User name",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black),
+                    ),
+                    Text(
+                      "Hace 2 horas",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 7.0,
+          ),
+          Text(
+            "Comentario Post Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            style: TextStyle(
+              fontSize: 15.0,
+              fontWeight: FontWeight.w400,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget textFormFielComment() {
+    return Positioned(
+      bottom: 0.0,
+      left: 0.0,
+      right: 0.0,
+      child: Container(
+        color: Colors.white,
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Agregar un comentario",
+                    border: InputBorder.none,
+                    filled: true,
+                    fillColor: Color.fromRGBO(240, 240, 240, 1),
+                    contentPadding: const EdgeInsets.only(
+                        left: 14.0, bottom: 8.0, top: 8.0),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(8),
+                      borderSide: new BorderSide(
+                        color: Color.fromRGBO(240, 240, 240, 1),
+                      ),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderRadius: new BorderRadius.circular(8),
+                      borderSide: new BorderSide(
+                        color: Color.fromRGBO(240, 240, 240, 1),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.send),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
