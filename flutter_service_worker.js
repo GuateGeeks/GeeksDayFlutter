8e26d620cfba7f9d6fc05bcff8367fa817418c63
adawolfs <adawolfs@gmail.com>
2021-09-11 19:06:59 +0000

    Deploying to gh-pages from @ GuateGeeks/GeeksDayFlutter@4a8bbbdddeb35b5513f345b305d316c0ca33c5e1 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,15 +3,15 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "77c7a05b95213406e2f1498a9c5461e5",
+  "main.dart.js": "3e2135e9bcc683393564839304100e7b",
 "version.json": "da2e0573834eb2187048bf7b88f15c46",
 "manifest.json": "cd30b6ab6774127656ea9c126369b699",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"index.html": "def919ae89fbaf4ed446d2adda969b52",
-"/": "def919ae89fbaf4ed446d2adda969b52",
-"assets/NOTICES": "65e220d389f5e2b5ce4b0566a3761c4e",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
+"index.html": "07d97533cf4654be828675efbab8444e",
+"/": "07d97533cf4654be828675efbab8444e",
+"assets/NOTICES": "648d292ef904316b685e8f67ef8c8b1d",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/AssetManifest.json": "03e86b6cae0fb1b6ad232ade2e6bc93d",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/assets/icon_facebook.png": "804cf2a905b2c7460adcaadb3dbfe9ae",
@@ -39,7 +39,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
