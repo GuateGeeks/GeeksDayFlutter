6e9656a3c9966415be9a2ad5b2db51636762bcae
Jhonproyec <albertcasti0919@gmail.com>
2021-10-29 16:26:04 -0600

    added event id in firestore_path file

--- lib/services/firestore_path.dart
+++ lib/services/firestore_path.dart
@@ -8,4 +8,5 @@ class FirestorePath {
   static String post(String postId) => 'posts/$postId';
   static String user(String userId) => 'users/$userId';
   static String quizRecords(String quizRecordsId) => 'quiz_records/$quizRecordsId';
+  static String events(String eventsId) => 'events/$eventsId';
 }
