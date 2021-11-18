ef8edf530e502b347f36b9a6fa57b35e1415d22e
Jhonproyec <72222081+Jhonproyec@users.noreply.github.com>
2021-11-18 18:43:10 +0000

    Deploying to gh-pages from @ GuateGeeks/GeeksDayFlutter@b351da235f140cb0835be2c993d5cfb295d4954b 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,27 +3,27 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"firebase-config.js": "01ebaf1b69d2a21a35fc2537dfa5deaf",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "b42bf6534a79a86897be5f91533658ec",
-"/": "b42bf6534a79a86897be5f91533658ec",
-"manifest.json": "cd30b6ab6774127656ea9c126369b699",
+  "manifest.json": "cd30b6ab6774127656ea9c126369b699",
 "version.json": "da2e0573834eb2187048bf7b88f15c46",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"main.dart.js": "78a443335f31407a04625f544dfcdd1c",
+"index.html": "671ac5bdd5c9270a74c3cfa3ff379785",
+"/": "671ac5bdd5c9270a74c3cfa3ff379785",
+"firebase-config.js": "01ebaf1b69d2a21a35fc2537dfa5deaf",
 "assets/NOTICES": "b80852edf30e021cec64cc3df1e65425",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/AssetManifest.json": "9bcdfe5b44b529e189098cbe7e717767",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/assets/icon.png": "8ac50d54716ef12d71db52324f32ba13",
-"assets/assets/icon_question.png": "f113f43571c39cb02a19246fb1663113",
-"assets/assets/icon_facebook.png": "804cf2a905b2c7460adcaadb3dbfe9ae",
-"assets/assets/logo.png": "7ae9268be56ac553cc479ece48e05991",
 "assets/assets/icon_google.png": "28b51fa69f1f0cb8a8f7a4bdd8e173c9",
 "assets/assets/icon_email.png": "37f207403ee19ea3f7b9e72200462eac",
+"assets/assets/logo.png": "7ae9268be56ac553cc479ece48e05991",
+"assets/assets/icon_question.png": "f113f43571c39cb02a19246fb1663113",
 "assets/assets/prueba.jpg": "07b405dbf42fbad21f4255a804c59cef",
-"assets/AssetManifest.json": "9bcdfe5b44b529e189098cbe7e717767",
-"main.dart.js": "b049e13f2a1b815dbe42c30448820730"
+"assets/assets/icon_facebook.png": "804cf2a905b2c7460adcaadb3dbfe9ae"
 };
 
 // The application shell files that are downloaded before a service worker can
