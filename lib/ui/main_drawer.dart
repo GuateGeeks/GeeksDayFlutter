694ad495854f6e924d36a2f1f99902ef87d9c9fb
Jhonproyec <albertcasti0919@gmail.com>
2021-12-02 16:48:14 -0600

    Ticket #38 show full avatar in menu

--- lib/ui/main_drawer.dart
+++ lib/ui/main_drawer.dart
@@ -18,15 +18,17 @@ class MainDrawer extends StatelessWidget {
     var userData = BlocProvider.of<AuthCubit>(context).getUser();
     return ListView(
       children: [
-        SizedBox(height: 60,),
-        Container(
-          color: Colors.transparent,
-          child: SvgPicture.string(
-            multiavatar(userData.image),
-            width: 140.0,  
+        Center(
+          child: Container(
+            margin: EdgeInsets.only(top: 50.0),
+            width: 130,
+            height: 130.0,
+            child: SvgPicture.string(
+              multiavatar(userData.image),
+            )
           ),
         ),
-        SizedBox(height: 5,),
+        SizedBox(height: 5.0),
         Center(
           child: Text(userData.name,
             style: Theme.of(context).textTheme.headline3,),
@@ -108,8 +110,118 @@ class MainDrawer extends StatelessWidget {
             "Logout",
             style: Theme.of(context).textTheme.headline6,
           ),
-        ),
-      ],
+        )
+      ]
     );
   }
 }
+
+// class MainDrawer extends StatelessWidget {
+//   const MainDrawer({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final authCubit = AuthCubit(AuthService());
+//     var userData = BlocProvider.of<AuthCubit>(context).getUser();
+//     return ListView(
+//       children: [
+//         SizedBox(height: 60,),
+//         Container(
+//           width: 20.0,
+//           color: Colors.yellow,
+//           child: CircleAvatar(
+//             radius: 65,
+
+//             child: SvgPicture.string(
+//               multiavatar(userData.image),
+              
+//             ),
+//           ),
+//         ),
+//         SizedBox(height: 5,),
+//         Center(
+//           child: Text(userData.name,
+//             style: Theme.of(context).textTheme.headline3,),
+//         ),
+//         SizedBox(height: 20.0),
+//         ListTile(
+//           onTap: () {
+//             Navigator.of(context).push(
+//               MaterialPageRoute(
+//                 builder: (context) {
+//                   return UserProfile();
+//                 },
+//               ),
+//             );
+//           },
+//           leading: Icon(Icons.person, color: Theme.of(context).iconTheme.color),
+//           title: Text(
+//             "Profile",
+//             style: Theme.of(context).textTheme.headline6,
+//           ),
+//         ),
+//         ListTile(
+//           onTap: () {
+//             Navigator.of(context).push(
+//               MaterialPageRoute(
+//                 builder: (context) {
+//                   return EventsCreate();
+//                 },
+//               ),
+//             );
+//           },
+//           leading: Icon(Icons.event_available, color: Theme.of(context).iconTheme.color),
+//           title: Text(
+//             "Eventos",
+//             style: Theme.of(context).textTheme.headline6,
+//           ),
+//         ),
+//         userData.isadmin == true ?
+//         ListTile(
+//           onTap: () {
+//             Navigator.of(context).push(
+//               MaterialPageRoute(
+//                 builder: (context) {
+//                   return AdminMetric();
+//                 },
+//               ),
+//             );
+//           },
+//           leading: Icon(Icons.admin_panel_settings, color: Theme.of(context).iconTheme.color),
+//           title: Text(
+//             "Admin",
+//             style: Theme.of(context).textTheme.headline6,
+//           ),
+//         )
+//         : Container(),
+//         ListTile(
+//           onTap: () {
+//             Navigator.of(context).push(
+//               MaterialPageRoute(
+//                 builder: (context) {
+//                   return Settings();
+//                 },
+//               ),
+//             );
+//           },
+//           leading:
+//               Icon(Icons.settings, color: Theme.of(context).iconTheme.color),
+//           title: Text(
+//             "Settings",
+//             style: Theme.of(context).textTheme.headline6,
+//           ),
+//         ),
+//         ListTile(
+//           onTap: () {
+//             authCubit.signOut();
+//           },
+//           leading: Icon(Icons.logout, color: Theme.of(context).iconTheme.color),
+//           title: Text(
+//             "Logout",
+//             style: Theme.of(context).textTheme.headline6,
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
