2d4bfa6126ba6046825f63d81c206a98c214df18
Jhonproyec <72222081+Jhonproyec@users.noreply.github.com>
2021-12-15 21:29:46 +0000

    Deploying to gh-pages from @ GuateGeeks/GeeksDayFlutter@d55d08fc4be31b43f91b2be808efd86075643d42 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "a4c7d8e5f3709e4c95c03cc82b153154",
+  "main.dart.js": "9bc6c3a87338baa47b6d25b3c53e3ebe",
 "version.json": "da2e0573834eb2187048bf7b88f15c46",
 "firebase-config.js": "01ebaf1b69d2a21a35fc2537dfa5deaf",
 "manifest.json": "cd30b6ab6774127656ea9c126369b699",
@@ -21,8 +21,8 @@ const RESOURCES = {
 "assets/assets/icon_question.png": "f113f43571c39cb02a19246fb1663113",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"index.html": "26505744c8790fc9c5af9856b79cb3de",
-"/": "26505744c8790fc9c5af9856b79cb3de",
+"index.html": "8057c521c8fc5c17d6f90f9dd62f4888",
+"/": "8057c521c8fc5c17d6f90f9dd62f4888",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796"
 };
 
