5f8b57309e65a99c5f86cbc1a278d779c0c3b053
Jhonproyec <albertcasti0919@gmail.com>
2022-02-14 17:36:47 -0600

    Ticket #65 Redesign creation of new quiz

--- lib/ui/post/quiz_create.dart
+++ lib/ui/post/quiz_create.dart
@@ -21,11 +21,12 @@ class QuizCreate extends StatefulWidget {
 class _QuizCreateState extends State<QuizCreate> {
   File? uploadedImage;
   Map<int, String> answersMap = {};
-    bool status = false;
+  bool status = false;
 
   @override
   Widget build(BuildContext context) {
-
+    double width = MediaQuery.of(context).size.width;
+    double maxWidth = width > 800 ? 800 : width;
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.white),
@@ -37,201 +38,153 @@ class _QuizCreateState extends State<QuizCreate> {
       ),
       body: BlocProvider(
         create: (_) => FeedCubit(PostService(), widget.event.id),
-        child: Container(
-          padding: EdgeInsets.fromLTRB(45, 25, 45, 5),
-          alignment: Alignment.topCenter,
-          child: createQuizBody(uploadedImage),
+        child: Center(
+          child: Container(
+            width: maxWidth,
+            padding: EdgeInsets.fromLTRB(20, 25, 20, 5),
+            child: createQuizBody(uploadedImage),
+          ),
         ),
       ),
     );
   }
 
   Widget createQuizBody(uploadedImage) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          image(uploadedImage),
-          description(),
-          SizedBox(
-            height: 30,
-          ),
-          inputAnswers(context)
-          // savePost(),
-        ],
-      ),
+    return Column(
+      children: [
+        descriptionAndImage(uploadedImage),
+        SizedBox(
+          height: 40,
+        ),
+        Divider(
+          thickness: 1,
+          height: 1.5,
+          color: Color(0xFFE5E5E5),
+        ),
+        SizedBox(
+          height: 80,
+        ),
+        inputAnswers(context),
+        inputAnswers(context),
+        inputAnswers(context),
+      ],
     );
   }
 
-  Widget image(uploadedImage) {
-   return Container(
-      height: 260,
-      width: 240,
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: uploadedImage == null
-                ? Container(
-                    width: 230,
-                    height: 230,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      border: Border.all(
-                        color: Color(0xFFD3D3D3),
-                      ),
-                    ),
-                    child: Icon(
-                      Icons.image,
-                      size: 80,
-                      color: Color(0xFFD3D3D3),
-                    ),
-                  )
-                : Container(
-                    width: 230,
-                    height: 230,
-                    child: PreviewImage(uploadedImage: uploadedImage),
-                  ),
-          ),
-          Positioned(
-            right: 5,
-            bottom: 10,
-            child: GestureDetector(
-              onTap: () => uploadImage(context),
-              child: Container(
-                width: 55,
-                height: 55,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).scaffoldBackgroundColor,
-                  borderRadius: BorderRadius.all(Radius.circular(50)),
-                  border: Border.all(
-                    color: Color(0xFFD3D3D3)
-                  ),
-                ),
-                child: Icon(
-                  Icons.camera_alt,
-                  size: 40,
-                  color: Color(0xFF0E89AF),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+  Widget descriptionAndImage(uploadedImage) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 7,
+          child: description(),
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        Expanded(
+          flex: 3,
+          child: image(uploadedImage),
+        )
+      ],
     );
   }
 
   Widget description() {
-     return PhysicalModel(
-      borderRadius: BorderRadius.circular(15),
-      color: Colors.white,
-      elevation: 5.0,
-      shadowColor: Colors.black,
+    return TextFormField(
+      maxLines: 4,
+      decoration:
+          InputDecoration(hintText: "Descripcion", border: InputBorder.none),
+    );
+  }
+
+  Widget image(uploadedImage) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: uploadedImage == null
+              ? Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    border: Border.all(
+                      color: Color(0xFFD3D3D3),
+                    ),
+                  ),
+                  child: Icon(
+                    Icons.image,
+                    size: 40,
+                    color: Color(0xFFD3D3D3),
+                  ),
+                )
+              : Container(
+                  width: 130,
+                  height: 130,
+                  child: PreviewImage(uploadedImage: uploadedImage),
+                ),
+        ),
+        Positioned(
+          right: 5,
+          bottom: -10,
+          child: GestureDetector(
+            onTap: () => uploadImage(context),
+            child: Container(
+              width: 35,
+              height: 35,
+              decoration: BoxDecoration(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+                border: Border.all(color: Color(0xFFD3D3D3)),
+              ),
+              child: Icon(
+                Icons.camera_alt,
+                size: 25,
+                color: Color(0xFF0E89AF),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget inputAnswers(BuildContext context) {
+    bool isSwitchOn = true;
+    
+    return Padding(
+      padding: const EdgeInsets.only(top: 10.0),
       child: TextFormField(
-        
-        maxLines: 6,
-        minLines: 6,
+        keyboardType: TextInputType.text,
         decoration: InputDecoration(
-          hintText: "Descripcion",
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide.none,
-            borderRadius: BorderRadius.circular(10),
+          suffixIcon: Switch(
+
+            value: isSwitchOn,
+            onChanged: (value) {
+              setState(() {
+                isSwitchOn = !isSwitchOn;
+              });
+            },
+          
           ),
-          border: UnderlineInputBorder(
-            borderSide: BorderSide.none,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          hintStyle: Theme.of(context).inputDecorationTheme.hintStyle,
-          fillColor: Theme.of(context).inputDecorationTheme.fillColor,
-          filled: true,
+          hintText: "Respuesta",
+          border: InputBorder.none,
+          filled: false,
         ),
       ),
     );
   }
 
-
-  Widget inputAnswers(BuildContext context) {
-    return Container(
-      child: Column(  
-        children: [
-          TextFormField(
-            decoration: InputDecoration(  
-              filled: false,
-              
-              prefixIcon: FlutterSwitch(
-                      width: 50.0,
-                      height: 20.0,
-                      toggleSize: 25.0,
-                      value: status,
-                      borderRadius: 70.0,
-                      padding: 2.0,
-                      toggleColor: Color.fromRGBO(2, 107, 206, 1),
-                      switchBorder: status ? Border.all(
-                        color: Color.fromRGBO(2, 107, 206, 1),
-                        width: 1.0,
-                      ) : Border.all(
-                        color: Color.fromRGBO(255, 255, 255, 1),
-                        width: 1.0,
-                      ),
-                      toggleBorder: Border.all(
-                        color: Color.fromRGBO(2, 107, 206, 1),
-                        width: 3.0,
-                      ),
-                      activeColor: Colors.black38,
-                      inactiveColor: Colors.black38,
-                      onToggle: (val) {
-                        setState(() {
-                          status = val;
-                        });
-                      },
-                    ),
-            ),
-          ),
-        ],
-      ),
-    );
-    return Padding(
-              padding: const EdgeInsets.only(top: 10.0),
-              child: TextFormField(
-                onChanged: (value) {
-                  },
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  suffixIcon: InkWell(
-                    onTap: () {
-                     
-                    },
-                    child: Tooltip(
-                      message: "Respuesta Correcta",
-                      
-                    ),
-                  ),
-                  hintText: "Agregar Respuesta",
-                  border: InputBorder.none,
-                  filled: true,
-                  fillColor: Theme.of(context).inputDecorationTheme.fillColor,
-                  contentPadding:
-                      const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
-                  focusedBorder:
-                      Theme.of(context).inputDecorationTheme.focusedBorder,
-                  enabledBorder:
-                      Theme.of(context).inputDecorationTheme.enabledBorder,
-                ),
-              ),
-            );
-        
-    
-  }
-
-    void saveAnswers(BuildContext context, int index, String value){
+  void saveAnswers(BuildContext context, int index, String value) {
     var postCubit = BlocProvider.of<PostCubit>(context);
     answersMap[index] = value;
-    answersMap.forEach((key, value) { 
+    answersMap.forEach((key, value) {
       setState(() {
         postCubit.updateQuizAnswer(key, value);
       });
     });
   }
-  
 
   uploadImage(BuildContext context) async {
     var uploadInput = FileUploadInputElement()..accept = 'image/*';
